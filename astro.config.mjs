89edc962cc0552b04660aadd2dd656620b9fbcd3
Leokratis <leokratisbak@gmail.com>
2025-05-02 20:00:16 +0300

    Admin panel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,25 +7,20 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
 
+import auth from "auth-astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nomy.vercel.app",
-  integrations: [
-    react(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "github-dark-dimmed",
-      },
-      gfm: true,
-    }),
-    icon(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    simpleStackForm(),
-  ],
+  integrations: [react(), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-dimmed",
+    },
+    gfm: true,
+  }), icon(), sitemap(), tailwind({
+    applyBaseStyles: false,
+  }), simpleStackForm(), auth()],
   output: "hybrid",
   adapter: vercel({
     analytics: true,
